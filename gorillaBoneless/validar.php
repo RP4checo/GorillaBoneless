d5a6a968dd29f6105aedf76287411f8ca570abb0
Twicez <hectorgrajales479@gmail.com>
2023-04-01 22:29:20 -0700

    Conexio/Login

---
+++ gorillaBoneless/validar.php
@@ -0,0 +1,27 @@
+<?php
+include('db.php');
+$correo=$_POST['correo'];
+$password=$_POST['password'];
+session_start();
+$_SESSION['correo']=$correo;
+
+
+$conexion=mysqli_connect("localhost","root","","gorillaz");
+
+$consulta="SELECT*FROM registro where correo='$correo' and password='$password'";
+$resultado=mysqli_query($conexion,$consulta);
+
+$filas=mysqli_num_rows($resultado);
+
+if($filas){
+  
+    header("location:carrito.html");
+
+}else{
+    ?>
+    <?php
+    include("login.html");
+
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
