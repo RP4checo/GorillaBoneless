17c700cb9fc71a33e4b26cb336207ea7fa2f8ed3
Aldo Mendivil <aldomendivil17@gmail.com>
2023-04-02 18:10:24 -0700

    Se agregó maquetado CRUD para productos

--- gorillaBoneless/js/main.js
+++ gorillaBoneless/js/main.js
@@ -117,3 +117,64 @@
     
 })(jQuery);
 
+
+//CRUD JS
+let cont = 004;
+let numeroConCeros = String(cont).padStart(3, '0');
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();
+	var actions = $("table td:last-child").html();
+	// Append table with add row form on add new button click
+    $(".add-new").click(function(){
+		$(this).attr("disabled", "disabled");
+		var index = $("table tbody tr:last-child").index();
+        var row = '<tr>' +
+            '<td>'+numeroConCeros+'</td>'+
+            '<td><input type="text" class="form-control" name="Nombre" id="Nombre"></td>' +
+            '<td><input type="text" class="form-control" name="Descripción" id="Descripción"></td>' +
+            '<td><input type="text" class="form-control" name="Precio" id="Precio"></td>' +
+            '<td><input type="text" class="form-control" name="Categoria" id="Categoria"></td>' +
+			'<td>' + actions + '</td>' +
+        '</tr>';
+    	$("table").append(row);		
+		$("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
+        $('[data-toggle="tooltip"]').tooltip();
+        cont++;
+        numeroConCeros = String(cont).padStart(3, '0');
+    });
+	// Add row on add button click
+	$(document).on("click", ".add", function(){
+		var empty = false;
+		var input = $(this).parents("tr").find('input[type="text"]');
+        input.each(function(){
+			if(!$(this).val()){
+				$(this).addClass("error");
+				empty = true;
+			} else{
+                $(this).removeClass("error");
+            }
+		});
+		$(this).parents("tr").find(".error").first().focus();
+		if(!empty){
+			input.each(function(){
+				$(this).parent("td").html($(this).val());
+			});			
+			$(this).parents("tr").find(".add, .edit").toggle();
+			$(".add-new").removeAttr("disabled");
+		}		
+    });
+	// Edit row on edit button click
+	$(document).on("click", ".edit", function(){		
+        $(this).parents("tr").find("td:not(:last-child):not(:first-child)").each(function(){
+            $(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
+        });		
+        $(this).parents("tr").find(".add, .edit").toggle();
+        $(".add-new").attr("disabled", "disabled");
+    });
+	// Delete row on delete button click
+	$(document).on("click", ".delete", function(){
+        $(this).parents("tr").remove();
+		$(".add-new").removeAttr("disabled");
+    });
+});
+
